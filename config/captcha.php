6265ccc0ebaeeec1e9b539d4cfbada284531117e
nckrtl <nick.retel@gmail.com>
2025-07-05 14:18:46 +0200

    wip

--- config/captcha.php
+++ config/captcha.php
@@ -1,15 +1,15 @@
 <?php
 
 return [
-    'service' => 'Recaptcha', // options: Recaptcha / Hcaptcha / Turnstile / Altcha
+    'service' => 'Turnstile', // options: Recaptcha / Hcaptcha / Turnstile / Altcha
     'sitekey' => env('CAPTCHA_SITEKEY', ''),
     'secret' => env('CAPTCHA_SECRET', ''),
     'collections' => [],
-    'forms' => ['all'],
+    'forms' => 'all',
     'user_login' => true,
     'user_registration' => true,
     'disclaimer' => '',
-    'invisible' => true,
+    'invisible' => false,
     'hide_badge' => true,
     'enable_api_routes' => false,
     'custom_should_verify' => null,
